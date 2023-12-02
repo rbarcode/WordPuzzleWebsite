041f59bf455bea71360249bf8a7c9f92e145b186
Richard Barbour <richyb07@gmail.com>
2023-12-01 16:55:43 -0800

    Update code behinds Register and Login pages, update Logout page.

--- CapstoneBlazorServerSite/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ CapstoneBlazorServerSite/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -1,12 +1,49 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
 
 namespace CapstoneBlazorServerSite.Areas.Identity.Pages.Account
 {
     public class LoginModel : PageModel
     {
+        private readonly SignInManager<IdentityUser> _signInManager;
+
+        public LoginModel(SignInManager<IdentityUser> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+
+        [BindProperty]
+        public InputModel Input { get; set; }
+        public string ReturnUrl { get; set; }
+
         public void OnGet()
         {
+            ReturnUrl = Url.Content("~/");
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            ReturnUrl = Url.Content("~/");
+
+            if (ModelState.IsValid)
+            {
+                var result = await _signInManager.PasswordSignInAsync(Input.Email, Input.Password, false, lockoutOnFailure: false);
+                if (result.Succeeded) return LocalRedirect(ReturnUrl);
+            }
+            return Page();
+        }
+
+        public class InputModel
+        {
+            [Required]
+            [EmailAddress]
+            public string Email { get; set; }
+
+            [Required]
+            [DataType(DataType.Password)]
+            public string Password { get; set; }
         }
     }
 }
--- CapstoneBlazorServerSite/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ CapstoneBlazorServerSite/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -1,12 +1,61 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace CapstoneBlazorServerSite.Areas.Identity.Pages.Account
 {
     public class RegisterModel : PageModel
     {
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public RegisterModel(SignInManager<IdentityUser> signInManager, UserManager<IdentityUser> userManager)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+
+        }
+
+        [BindProperty]
+        public InputModel Input { get; set; }
+        public string ReturnUrl { get; set; }
+
         public void OnGet()
         {
+            ReturnUrl = Url.Content("~/");
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            ReturnUrl = Url.Content("~/");
+            if(ModelState.IsValid)
+            {
+                var identity = new IdentityUser { UserName = Input.Name, Email = Input.Email };
+                var result = await _userManager.CreateAsync(identity, Input.Password);
+
+                if (result.Succeeded)
+                {
+                    await _signInManager.SignInAsync(identity, isPersistent: false);
+                    return LocalRedirect(ReturnUrl);
+                }
+            }
+            return Page();
+        }
+
+        public class InputModel
+        {
+            [Required]
+            public string Name { get; set; }
+
+            [Required]
+            [EmailAddress]
+            public string Email { get; set; }
+
+            [Required]
+            [DataType(DataType.Password)]
+            public string Password { get; set; }
         }
     }
 }
