8815f20c1ffe3f47c758aab8fe9be6eac993ae2f
Richard Barbour <richyb07@gmail.com>
2023-12-08 13:37:40 -0800

    Add CareerStats model for database table.

---
+++ CapstoneBlazorServerSite/Models/CareerStats.cs
@@ -0,0 +1,33 @@
+﻿namespace CapstoneBlazorServerSite.Models
+{
+    public class CareerStats
+    {
+        public int CareerStatsId { get; set; }
+
+        public string PlayerId { get; set; }
+
+        public ApplicationUser Player { get; }
+
+        public uint CareerScore { get; set; }
+
+        public uint CareerMinutesPlayed { get; set; }
+
+        private float _pointsPerMinute;
+
+        public uint CareerNumberOfWords { get; set; }
+
+        private float _pointsPerWord;
+
+        public float PointsPerMinute
+        {
+            get => _pointsPerMinute;
+            set => _pointsPerMinute = (CareerScore / CareerMinutesPlayed);
+        }
+
+        public float PointsPerWord
+        {
+            get => _pointsPerWord;
+            set => _pointsPerWord = (CareerScore / CareerNumberOfWords);
+        }
+    }
+}
