c84025828c9c44727783078f25de5bb63d630741
Richard Barbour <richyb07@gmail.com>
2023-12-14 20:05:35 -0800

    Add NavBarComponent, update Leaderboards page, add image assets.

---
+++ CapstoneBlazorServerSite/Components/NavBarComponent.razor.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace CapstoneBlazorServerSite.Components
+{
+    public partial class NavBarComponent
+    {
+        [Parameter]
+        public bool IsAuthenticated { get; set; }
+    }
+}
