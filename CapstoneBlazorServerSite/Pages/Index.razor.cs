661b5736201b71f2fcd12fbde47850398ee4873c
Richard Barbour <richyb07@gmail.com>
2023-12-19 00:52:40 -0800

    Add JavaScript module and user interactivity to Index.razor.

---
+++ CapstoneBlazorServerSite/Pages/Index.razor.cs
@@ -0,0 +1,31 @@
+﻿
+
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace CapstoneBlazorServerSite.Pages
+{
+    public partial class Index : IAsyncDisposable
+    {
+        [Inject]
+        IJSRuntime JS {  get; set; }
+        
+        private IJSObjectReference module;
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            module = await JS.InvokeAsync<IJSObjectReference>("import", "./js/splashPageListener.js");
+            await module.InvokeVoidAsync("initializeSplashPageListener");
+        }
+
+        public async ValueTask DisposeAsync() 
+        { 
+            if(module != null)
+            {
+                await module.DisposeAsync();
+            }
+            
+        }
+
+    }    
+}
---
+++ CapstoneBlazorServerSite/wwwroot/js/splashPageListener.js
@@ -0,0 +1,16 @@
+﻿export function initializeSplashPageListener() {
+    document.addEventListener("mousemove", parallax);
+    function parallax(e) {
+        let w = window.innerWidth / 2;
+        let h = window.innerHeight / 2;
+        let mouseX = e.clientX;
+        let mouseY = e.clientY;
+        let depth1 = `${50 + (mouseX - w) * 0.01}% ${50 + (mouseY - h) * 0.02}%`;
+        let depth2 = `${50 + (mouseX - w) * 0.02}% ${50 + (mouseY - h) * 0.04}%`;
+        let depth3 = `${50 + (mouseX - w) * 0.04}% ${50 + (mouseY - h) * 0.08}%`;
+        let x = `${depth3},${depth2},${depth1}`;
+
+        const elem = document.querySelector(".parallax");
+        elem.style.backgroundPosition = x;
+    }
+}
