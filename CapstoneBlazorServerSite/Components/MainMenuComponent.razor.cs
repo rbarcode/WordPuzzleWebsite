fc659947c84523bb3896d646fa7f609e3d8fece9
Richard Barbour <richyb07@gmail.com>
2023-12-19 11:11:50 -0800

    Add authentication-based procedural logic to Index.razor and MainMenu component.

---
+++ CapstoneBlazorServerSite/Components/MainMenuComponent.razor.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace CapstoneBlazorServerSite.Components
+{
+    public partial class MainMenuComponent
+    {
+        [Parameter]
+        public bool IsAuthenticated { get; set; }
+    }
+}
--- CapstoneBlazorServerSite/Pages/Index.razor.cs
+++ CapstoneBlazorServerSite/Pages/Index.razor.cs
@@ -1,21 +1,38 @@
-﻿
-
-using Microsoft.AspNetCore.Components;
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.JSInterop;
 
 namespace CapstoneBlazorServerSite.Pages
 {
     public partial class Index : IAsyncDisposable
     {
+        [CascadingParameter]
+        private Task<AuthenticationState>? AuthenticationStateTask { get; set; }
+
+        public bool IsAuthenticated { get; set; }
+
         [Inject]
         IJSRuntime JS {  get; set; }
         
         private IJSObjectReference module;
 
+        protected override async Task OnInitializedAsync()
+        {
+            await CheckAuth();
+        }
+
         protected override async Task OnAfterRenderAsync(bool firstRender)
         {
             module = await JS.InvokeAsync<IJSObjectReference>("import", "./js/splashPageListener.js");
             await module.InvokeVoidAsync("initializeSplashPageListener");
+        }
+
+        private async Task CheckAuth()
+        {
+            if (await AuthenticationStateTask != null) 
+            {
+                IsAuthenticated = true;
+            }
         }
 
         public async ValueTask DisposeAsync() 
