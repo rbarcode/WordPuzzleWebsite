785b134d33ac57e4f4ea28f954d349861fb2cc1e
Richard Barbour <richyb07@gmail.com>
2023-12-18 21:43:45 -0800

    Complete full functionality and styling for LoginRegister razor page.

---
+++ CapstoneBlazorServerSite/wwwroot/js/loginListerner.js
@@ -0,0 +1,12 @@
+﻿const wrapper = document.querySelector(".wrapper");
+const loginLink = document.querySelector(".login-link");
+const registerLink = document.querySelector(".register-link");
+
+registerLink.onclick = () => {
+    wrapper.classList.add("active");
+}
+
+
+loginLink.onclick = () => {
+    wrapper.classList.remove("active");
+}
