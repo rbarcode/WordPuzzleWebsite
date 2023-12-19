1d82c7aa0aaf9f0477546a8565ef3ad3ce226c3e
Richard Barbour <richyb07@gmail.com>
2023-12-19 13:42:39 -0800

    Clean up code in Index.razor.cs.

--- CapstoneBlazorServerSite/Pages/Index.razor.cs
+++ CapstoneBlazorServerSite/Pages/Index.razor.cs
@@ -6,34 +6,17 @@ namespace CapstoneBlazorServerSite.Pages
 {
     public partial class Index : IAsyncDisposable
     {
-        [CascadingParameter]
-        private Task<AuthenticationState>? AuthenticationStateTask { get; set; }
-
-
-        public bool IsAuthenticated { get; set; }
 
         [Inject]
         IJSRuntime JS {  get; set; }
         
         private IJSObjectReference module;
 
-        protected override async Task OnInitializedAsync()
-        {
-            await CheckAuth();
-        }
-
         protected override async Task OnAfterRenderAsync(bool firstRender)
         {
             module = await JS.InvokeAsync<IJSObjectReference>("import", "./js/splashPageListener.js");
             await module.InvokeVoidAsync("initializeSplashPageListener");
-        }
-
-        private async Task CheckAuth()
-        {
-            if (await AuthenticationStateTask != null) 
-            {
-                IsAuthenticated = true;
-            }
+            Console.WriteLine("splashPageListener loaded!");
         }
 
         public async ValueTask DisposeAsync() 
@@ -41,6 +24,7 @@ namespace CapstoneBlazorServerSite.Pages
             if(module != null)
             {
                 await module.DisposeAsync();
+                Console.WriteLine("splashPageListener disposed!");
             }
             
         }
